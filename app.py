a2d6afd1265828a1434a6e6f12417bf2a1cffb61
jingt06 <cs.jingtao@gmail.com>
2020-11-22 19:53:54 -0500

    set up webpack and react

--- app.py
+++ app.py
@@ -6,20 +6,32 @@ import json
 
 def create_app(test_config = None):
     #create and configure the app
-    with open('config.json') as f:
+    with open("config.json") as f:
         config = json.load(f)
     initialize_mongo(config["mongo_config"])
-    app = Flask(__name__)
+    app = Flask(
+        __name__,
+        template_folder="frontend/templates",
+        static_folder="frontend/dist",
+    )
+
+    @app.route("/")
+    def react():
+        return render_template("base.html")
+
+    @app.route("/bundle.js")
+    def dist_file():
+        return app.send_static_file("bundle.js")
 
     # a simple page that says hello
-    @app.route('/home')
+    @app.route("/home")
     def hello():
-        return 'Home Page'
+        return "Home Page"
 
     from api import generate_grocery_list
     app.register_blueprint(generate_grocery_list.bp)
 
-    @app.route('/ingredient-library')
+    @app.route("/ingredient-library")
     def get_ingredient():
         from document.ingredient import Ingredient
         ingredients = Ingredient.get_ingredients()
@@ -27,18 +39,4 @@ def create_app(test_config = None):
         for ingredient in ingredients:
             buffer += ingredient.display()
         return buffer
-
-
     return app
-
-
-
-#
-# @app.route('/')
-# def hello_world():
-#     return 'Hello, World!'
-#
-#
-# @app.route('/get_meal')
-# def get_meal():
-#     return jsonify({"name":"Chicken with Broccoli","ingredient":["Chicken Breast","Broccoli"]}, {"name": "To
